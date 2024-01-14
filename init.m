579430414696ea688251bceafee3f5b3fa51eab4
VittoRose <135705006+VittoRose@users.noreply.github.com>
2024-01-14 10:38:30 +0100

    Kalman gain init

--- init.m
+++ init.m
@@ -8,9 +8,6 @@ out_rate = 0.001;   %[s]
 
 %% Sensors carateristics
 
-% C state space matric
-C = [1 0 0 0; 0 1 0 0];
-
 % Sampling times
 Ts.y1 = 0.03;       %[s]
 Ts.y2 = 0.04;       %[s]
@@ -21,14 +18,6 @@ std_dev.y2 = 0.09;  %[rad]
 
 % Sensors covariance R
 R = blkdiag(std_dev.y1^2, std_dev.y2^2);
-
-%% Kalman filter
-
-% Selector param
-KALMAN_FILTER = true;       % True for KF, false for UKF
-
-dt = 0.001;                 %[s] Sampling time of UKF
-m_treshold = 5;
 
 %% Ideal Initial conditions
 
@@ -47,12 +36,12 @@ P_0 = zeros(4);%blkdiag(std_dev.z_0^2, std_dev.theta_0^2, std_dev.dz_0^2, std_de
 
 %% Ideal System params
 
-s_param.J = 5000;     %[kgm^2]
-s_param.m = 2000;     %[kg]
-s_param.b = 150;      %[Ns/m]
-s_param.beta = 15;    %[Nms/rad]
-s_param.g = 9.81;     %[m/s^2]
-s_param.l = 10;       %[m]
+s_param.J = 5000;     % [kgm^2]
+s_param.m = 2000;     % [kg]
+s_param.b = 150;      % [Ns/m]
+s_param.beta = 15;    % [Nms/rad]
+s_param.g = 9.81;     % [m/s^2]
+s_param.l = 10;       % [m]
 
 %% Input parameters
 
@@ -60,3 +49,44 @@ std_dev.fm = 0.01;                          % fm standard deviation
 std_dev.fa = 0.01;                          % fa standard deviation 
 Q = blkdiag(std_dev.fm^2, std_dev.fa^2);    % Disturbe process covariance
 U_mean= [0; 0];                             % Mean disturbe process
+
+%% Linear sys
+
+% Working point
+z_w = 0;
+theta_w = 0;
+dz_w = 0;
+dtheta_w = 0;
+
+fm_w = 0;
+fa_w = 0;
+
+% State space matrix
+A = [0 0 1 0;...
+    0 0 0 1;...
+    -s_param.b/s_param.m -fm_w*sin(theta_w)/s_param.m 0 0;...
+    0 0 0 -s_param.b/s_param.J];
+
+B = [0 0;...
+    0 0;...
+    cos(theta_w) 0;
+    0 2*s_param.l/s_param.J];                   % u = [fm;fa]
+
+C = [1 0 0 0;...
+    0 1 0 0];
+
+D = zeros(2,2);
+
+lin_sys = ss(A,B,C,D);
+
+%% Kalman filter
+
+% Selector param
+KALMAN_FILTER = true;       % True for KF, false for UKF
+
+% KF matrix
+[~, Kf] = kalman(lin_sys, Q,R);
+
+% UKF param
+dt = 0.001;                 % [s] Sampling time of UKF
+m_treshold = 5;
