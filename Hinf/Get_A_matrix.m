ea7fb5f90bb8454f593778ebdf8513549f364370
FP98 <131855491+FP98@users.noreply.github.com>
2024-01-17 19:17:34 +0100

    Hinf

---
+++ Hinf/Get_A_matrix.m
@@ -0,0 +1,22 @@
+%% Rename params
+J = s_param.J;     
+m = s_param.m;     
+b = s_param.b;      
+beta = s_param.beta;    
+g = s_param.g;     
+l = s_param.l;   
+
+syms z theta dz dtheta fm fa;
+
+x = [z, theta, dz, dtheta];
+f = [dz; dtheta; -b/m * dz + fm/m * cos(theta)
+    - g; -beta/J * dtheta + 2 * l/J * (fa)];
+
+a = jacobian(f(1), x);
+b = jacobian(f(2), x);
+c = jacobian(f(3), x);
+d = jacobian(f(4), x);
+
+A = [a;b;c;d];
+
+matlabFunction(A,'file','A_matrix', 'Vars', {x,fm, fa});
